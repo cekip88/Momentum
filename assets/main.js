ddb24487290792e0ff6358036edca33798e7c72f
cekip88 <cekip@mail.ru>
2020-10-22 02:32:54 +0600

    Поправил анимацию смены фонов, реализована работа тасков, осталось сделать погоду и цитаты

--- assets/main.js
+++ assets/main.js
@@ -127,7 +127,19 @@ class Momentum {
 
     setBgPicture(i){
         const _ = this;
-        _.bgcTag.setAttribute('style',`background-image:url(assets/${_.bgc[i]})`);
+        let bgcInnerTag = document.createElement('DIV');
+        bgcInnerTag.setAttribute('style',`background-image:url(assets/${_.bgc[i]})`);
+        _.bgcTag.prepend(bgcInnerTag);
+
+        if(_.bgcTag.children.length > 1){
+            let last = _.bgcTag.lastChild;
+            let styles = last.getAttribute('style');
+            styles += ';opacity:0;transform:scale(1.15)';
+            last.setAttribute('style',`${styles}`);
+            setTimeout(function (e) {
+                last.remove();
+            },1000)
+        }
     }
     setNextBgc(){
         const _ = this;
