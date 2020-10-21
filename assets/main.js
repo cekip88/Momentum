9befdf04df22365e17bb285c4d5f9a0dccb3f908
cekip88 <cekip@mail.ru>
2020-10-22 02:13:39 +0600

    Удален перевод, реализована работа тасков, осталось сделать погоду

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 class Momentum {
+    path;
     constructor() {
         const _ = this;
         _.timeTag = document.querySelector('.time');
@@ -6,38 +7,104 @@ class Momentum {
         _.bgcTag = document.querySelector('.background');
         _.hour = new Date().getHours();
         _.bgc = [
-            'night-01.jpg',
-            'night-02.jpg',
-            'night-03.jpg',
-            'night-04.jpg',
-            'night-05.jpg',
-            'night-06.jpg',
-            'morning-01.jpg',
-            'morning-02.jpg',
-            'morning-03.jpg',
-            'morning-04.jpg',
-            'morning-05.jpg',
-            'morning-06.jpg',
-            'day-01.jpg',
-            'day-02.jpg',
-            'day-03.jpg',
-            'day-04.jpg',
-            'day-05.jpg',
-            'day-06.jpg',
-            'evening-01.jpg',
-            'evening-02.jpg',
-            'evening-03.jpg',
-            'evening-04.jpg',
-            'evening-05.jpg',
-            'evening-06.jpg'];
+            'night-01.jpg', 'night-02.jpg', 'night-03.jpg', 'night-04.jpg', 'night-05.jpg', 'night-06.jpg',
+            'morning-01.jpg', 'morning-02.jpg', 'morning-03.jpg', 'morning-04.jpg', 'morning-05.jpg', 'morning-06.jpg',
+            'day-01.jpg', 'day-02.jpg', 'day-03.jpg', 'day-04.jpg', 'day-05.jpg', 'day-06.jpg',
+            'evening-01.jpg', 'evening-02.jpg', 'evening-03.jpg', 'evening-04.jpg', 'evening-05.jpg', 'evening-06.jpg'];
         _.nameInputTag = document.getElementById('name');
 
         _.init();
 
         document.getElementById('bgcList').addEventListener('click',function (e) {_.setNextBgc()});
-        document.querySelector('.burger').addEventListener('click',function () {_.burgerClick()});
-        document.getElementById('lang-btn-en').addEventListener('click',function (e) {_.changeLang('en')});
-        document.getElementById('lang-btn-ru').addEventListener('click',function (e) {_.changeLang('ru')});
+        document.getElementById('add').addEventListener('click',function (e) {
+            let btn = e.target;
+            _.addTask(btn)
+        });
+
+
+    }
+
+    getTasks(){
+        let tasks = localStorage.getItem('momentum-tasks');
+        if(!tasks) return null;
+        tasks = JSON.parse(tasks);
+        return tasks
+    }
+    saveTasks(tasks){
+        tasks = JSON.stringify(tasks);
+        localStorage.setItem('momentum-tasks',tasks)
+    }
+    showTasks(){
+        let tasks = this.getTasks();
+        if(!tasks) return;
+
+        let btn = document.getElementById('add');
+        for (let task in tasks){
+            this.addTask(btn,tasks[task],task)
+        }
+    }
+    saveTask(taskNameCont){
+        let tasks = this.getTasks();
+        if (!tasks) tasks = {};
+
+        if(!taskNameCont.hasAttribute('data-task')) taskNameCont.setAttribute('data-task',`${taskNameCont.textContent}`);
+        tasks[`${taskNameCont.getAttribute('data-task')}`] = taskNameCont.textContent;
+
+        this.saveTasks(tasks)
+    }
+    addHandlers(items){
+        items.taskName.addEventListener('focusout', function (e) {
+            let taskName = e.target;
+            if (taskName.textContent === '') taskName.textContent = this.taskValue;
+            else momentum.saveTask(taskName);
+        });
+        items.taskName.addEventListener('focus', function (e) {
+            let taskName = e.target;
+            this.taskValue = taskName.textContent;
+            setTimeout(function (e) {taskName.textContent = ''},10)
+        });
+        items.taskBtn.addEventListener('click',function (e) {
+            momentum.deleteTask(e);
+        })
+    }
+    addTask(btn, name = '[Enter your task]', attr = null){
+        let task = document.createElement('DIV'),
+            taskName = document.createElement('DIV'),
+            taskBtn = document.createElement('BUTTON');
+
+        task.className = 'task-row';
+        taskName.setAttribute('contenteditable','true');
+        taskName.textContent = name;
+        if(attr) taskName.setAttribute('data-task', attr);
+
+        let cont = document.querySelector('.target');
+        task.append(taskName,taskBtn);
+        cont.append(task);
+
+        if(cont.children.length === 6) btn.classList.add('add-hidden');
+        this.addHandlers({taskName:taskName,taskBtn:taskBtn})
+    }
+    deleteTask(e){
+        let btn = e.target,
+            task = btn.parentElement,
+            taskName = task.firstChild.getAttribute('data-task');
+
+        let tasks = this.getTasks();
+
+        delete tasks[taskName];
+        task.setAttribute('style','transform:scale(0)');
+        setTimeout(function (e) {
+            task.remove();
+        },350);
+
+        this.saveTasks(tasks);
+
+        if(document.querySelector('.target').children.length < 7){
+            if (document.getElementById('add').classList.contains('add-hidden')){
+                document.getElementById('add').classList.remove('add-hidden')
+            }
+        }
+
     }
 
     bgcCheck(){
@@ -50,30 +117,18 @@ class Momentum {
         }
     }
     setGreetings(hour){
-        const _ = this;
-
-        let greetings = _.lang === 'en' ? ['Good night,','Good morning,','Good day,','Good evening,'] : ['Доброй ночи,','Доброе утро,','Добрый день,','Добрый вечер,'];
+        let greetings = ['Good night,','Good morning,','Good day,','Good evening,'];
         let greetingArea = document.getElementById('greeting');
         if(hour >=0 && hour < 6) greetingArea.textContent = greetings[0];
         if(hour >=6 && hour < 12) greetingArea.textContent = greetings[1];
         if(hour >=12 && hour < 18) greetingArea.textContent = greetings[2];
         if(hour >=18 && hour <= 23) greetingArea.textContent = greetings[3];
     }
-    setTexts(){
-        const _ = this;
 
-        let buttonText = ['Add task','Change background','Choose language:'];
-        if(_.lang === 'ru') buttonText = ['Добавить задание','Сменить фон','Выберите язык:'];
-
-        document.getElementById('add').textContent = buttonText[0];
-        document.getElementById('bgcList').textContent = buttonText[1];
-        document.querySelector('.lang span').textContent = buttonText[2];
-    }
     setBgPicture(i){
         const _ = this;
         _.bgcTag.setAttribute('style',`background-image:url(assets/${_.bgc[i]})`);
     }
-
     setNextBgc(){
         const _ = this;
         let nextBgc = document.getElementById('bgcList');
@@ -108,7 +163,6 @@ class Momentum {
             year = _.dateLengthCheck(date.getFullYear() + '');
 
         let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-        if(_.lang === 'ru') days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
         day = days[day];
 
         let str = day + ', ' + monthDay + '.' + month + '.' + year;
@@ -124,7 +178,7 @@ class Momentum {
     nameHandler(){
         const _ = this;
 
-        if(!localStorage.getItem('momentum-name')) localStorage.setItem('momentum-name','[введите имя]');
+        if(!localStorage.getItem('momentum-name')) localStorage.setItem('momentum-name','[enter name]');
         _.nameInputTag.textContent = localStorage.getItem('momentum-name');
 
         function enterName(){
@@ -145,46 +199,18 @@ class Momentum {
 
     }
 
-    burgerClick(){
-        let block = document.querySelector('.right-top');
-        block.classList.toggle('right-top-active')
-    }
-
-    changeLang(lang){
-        const _ = this;
-        let date = new Date();
-        let hour = date.getHours();
-        let alternative = lang === 'en' ? 'ru' : 'en';
-        if(!document.querySelector('.lang').classList.contains(lang)) {
-            document.querySelector('.lang').classList.add(lang);
-            document.querySelector('.lang').classList.remove(alternative);
-            localStorage.setItem('momentum-lang',lang);
-            _.lang = lang;
-            _.setTexts();
-            _.setGreetings(hour);
-            _.getDate(date)
-        }
-    }
-    chooseLang(){
-        const _ = this;
-
-        if(!localStorage.getItem('momentum-lang')) localStorage.setItem('momentum-lang','en');
-        _.lang = localStorage.getItem('momentum-lang');
-        document.querySelector('.lang').classList.add(_.lang);
-    }
-
     init(){
         const _ = this;
 
-        _.chooseLang();
         _.setGreetings(_.hour);
         _.setBgPicture(_.hour);
-        _.setTexts();
         _.nameHandler();
+        _.insertTime();
         setInterval(function () {
             _.bgcCheck();
             _.insertTime();
         },1000);
+        _.showTasks();
     }
 }
 
